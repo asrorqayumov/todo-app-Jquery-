25ab441860e92d933a6195b2ed8e70625c2ce5de
Asror Qayumov <qayumovasror2003@gmail.com>
2022-03-25 18:59:42 +0500

    project finished

---
+++ app.js
@@ -0,0 +1,93 @@
+let database = [
+  {
+    id: 126599,
+    title: "Dangasalik qilish",
+    isChecked: true,
+    date: new Date(),
+    category: "Daily",
+  },
+];
+
+let form = document.forms[0];
+let todoList = document.querySelector(".todos");
+
+
+function ToDo(title, category) {
+  this.id = Math.round(Math.random() * (999999 - 100000) + 100000);
+  this.title = title;
+  this.date = new Date();
+  this.isChecked = false;
+  this.category = category;
+}
+
+form.onsubmit = (e) => {
+  let todo = new ToDo(form.title.value, form.category.value);
+  e.preventDefault();
+  AddToDo(todo);
+};
+
+function displayToDoList(database) {
+  let html = "";
+  database.forEach((todo) => {
+    html += `
+    <li class="list" id="${todo.id}">
+    <span class="todoTitle">
+    ${todo.title}
+  </span>
+  <span class="todoCategory">
+  ${todo.category}
+  </span>
+  <div>
+  <i id="edit" class="fa-solid fa-pen" onclick="editTodo(this)"></i> 
+  <i id="delete" class="fa-solid fa-trash"  onclick="deleteToDo(this)"></i> 
+  </div> 
+  </li>`;
+});
+todoList.innerHTML = html;
+}
+
+// Add
+
+function AddToDo(todo) {
+  database.push(todo);
+  todoList.innerHTML += `
+  <li class="list" id="${todo.id}">
+  <span class="todoTitle">
+  ${todo.title}
+  </span>
+  <span class="todoCategory">
+  ${todo.category}
+  </span>
+  <div>
+  <i id="edit" class="fa-solid fa-pen" onclick="editTodo(this)"></i> 
+  <i id="delete" class="fa-solid fa-trash"  onclick="deleteToDo(this)"> </i>
+  </div>  
+  </li>`;
+  form.title.value = "";
+}
+
+// Delete
+function deleteToDo(e) {
+  let id = e.parentElement.parentElement.id;
+  let filteredDatabase = database.filter((item) => {
+    return item.id != id;
+  });
+  database = filteredDatabase;
+  displayToDoList(database);
+}
+
+// Edit
+function editTodo(e) {
+  let id = e.parentElement.parentElement.id;
+  let findTodo = database.find((item) => {
+    return item.id == id;
+  });
+  let title = prompt("Edit a name:");
+  findTodo.title = title;
+  displayToDoList(database);
+}
+
+
+window.onload = () => {
+  displayToDoList(database);
+};
